32fe52f675c63916775f47244495e11f3e1549a3
Jory Graham <jory.graham@gmail.com>
2014-10-28 23:51:16 -0400

    A failing test case

--- test/spanners_test.js
+++ test/spanners_test.js
@@ -4,14 +4,23 @@ var assert = require('assert'),
     spanners = require('../lib/spanners.js'),
     jsdom = require('jsdom');
 
+var compare = function (input, output) {
+  jsdom.env(input, function (iErrors, iWindow) {
+    jsdom.env(output, function (oErrors, oWindow) {
+      var inner = spanners.squirrel(iWindow.document.body).innerHTML;
+      var outer = oWindow.document.body.innerHTML;
+      assert(inner === outer, input + " !== " + output);
+    });
+  });
+};
+
 describe('spanners node module.', function () {
 
-  it('no children, no childNodes', function() {
+  it('no children, no childNodes', function () {
+    compare('<br/>', '<br/>');
+  });
 
-    jsdom.env('<br/>',  function (errors, window) {
-
-      var node = window.document.body.firstChild;
-      assert(spanners.squirrel(node), node);
-    });
+  it('no children, a childNode', function () {
+    compare('<div>blah</div>', '<div><span>b</span><span>l</span><span>a</span><span>h</span></div>');
   });
 });
