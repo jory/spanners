0fb10593d283367e6e84e8ce7d7fe5678e4fb804
Jory Graham <jory.graham@gmail.com>
2014-10-30 01:09:44 -0400

    More tests! More passing!

--- test/spanners_test.js
+++ test/spanners_test.js
@@ -9,18 +9,35 @@ var compare = function (input, output) {
     jsdom.env(output, function (oErrors, oWindow) {
       var inner = spanners.squirrel(iWindow.document.body).innerHTML;
       var outer = oWindow.document.body.innerHTML;
-      assert(inner === outer, input + " !== " + output);
+      assert(inner === outer, inner);
     });
   });
 };
 
 describe('spanners node module.', function () {
 
-  it('no children, no childNodes', function () {
-    compare('<br/>', '<br/>');
+  it('children 0, childNodes 0', function () {
+    compare('<br/>',
+            '<br/>');
   });
 
-  it('no children, a childNode', function () {
-    compare('<div>blah</div>', '<div><span>b</span><span>l</span><span>a</span><span>h</span></div>');
+  it('children 0, childNodes 1', function () {
+    compare('<div>blah</div>',
+            '<div><span>b</span><span>l</span><span>a</span><span>h</span></div>');
+  });
+
+  it('children 1, childNodes 1', function () {
+    compare('<div><p>foo</p></div>',
+            '<div><p><span>f</span><span>o</span><span>o</span></p></div>');
+  });
+
+  it('children 1, childNodes 3', function () {
+    compare('<div>blah<p>foo</p>bar</div>',
+            '<div><span>b</span><span>l</span><span>a</span><span>h</span><p><span>f</span><span>o</span><span>o</span></p><span>b</span><span>a</span><span>r</span></div>');
+  });
+
+  it('children 2, childNodes 2', function () {
+    compare('<div><p>foo</p><p>bar</p></div>',
+            '<div><p><span>f</span><span>o</span><span>o</span></p><p><span>b</span><span>a</span><span>r</span></p></div>');
   });
 });
